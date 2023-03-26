ccd9f9399e66d7b8adf213dbdeb72ffc98b4c6f3
hhyeok1026 <hhyeok1026@gmail.com>
2023-03-26 10:37:35 +0900

    workmanager advanced 부분을 보았다. 코루틴, 플로우쪽을 잘 모르니까 어려운데, 추가적으로 컴포즈에 디자인패턴 들어가고 너무 어려운듯 하다.

--- android-basics-compose/Unit7/pathway1/workmanager-starter/app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
+++ android-basics-compose/Unit7/pathway1/workmanager-starter/app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
@@ -18,21 +18,25 @@ package com.example.bluromatic.data
 
 import android.content.Context
 import android.net.Uri
+import androidx.lifecycle.asFlow
 import androidx.work.*
-import com.example.bluromatic.KEY_BLUR_LEVEL
-import com.example.bluromatic.KEY_IMAGE_URI
-import com.example.bluromatic.getImageUri
+import com.example.bluromatic.*
 import com.example.bluromatic.workers.BlurWorker
 import com.example.bluromatic.workers.CleanupWorker
 import com.example.bluromatic.workers.SaveImageToFileWorker
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.mapNotNull
 
 class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
 
     private var imageUri: Uri = context.getImageUri()
     private val workManager = WorkManager.getInstance(context)
-    override val outputWorkInfo: Flow<WorkInfo?> = MutableStateFlow(null)
+    // override val outputWorkInfo: Flow<WorkInfo?> = MutableStateFlow(null)
+    override val outputWorkInfo: Flow<WorkInfo> =
+        workManager.getWorkInfosByTagLiveData(TAG_OUTPUT).asFlow().mapNotNull {
+            if (it.isNotEmpty()) it.first() else null
+        }
 
     /**
      * Create the WorkRequests to apply the blur and save the resulting image
@@ -40,16 +44,29 @@ class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
      */
     override fun applyBlur(blurLevel: Int) {
 
-        var continuation = workManager.beginWith(OneTimeWorkRequest.from(CleanupWorker::class.java))
+        // var continuation = workManager.beginWith(OneTimeWorkRequest.from(CleanupWorker::class.java))
+        var continuation = workManager
+            .beginUniqueWork(
+                IMAGE_MANIPULATION_WORK_NAME,
+                ExistingWorkPolicy.REPLACE,
+                OneTimeWorkRequest.from(CleanupWorker::class.java)
+            )
+
+        val constraints = Constraints.Builder()
+            .setRequiresBatteryNotLow(true)
+            .build()
 
         val blurBuilder = OneTimeWorkRequestBuilder<BlurWorker>()
         blurBuilder.setInputData(createInputDataForWorkRequest(blurLevel, imageUri))
+        blurBuilder.setConstraints(constraints)
 
         // workManager.enqueue(blurBuilder.build())
         continuation = continuation.then(blurBuilder.build())
 
         val save = OneTimeWorkRequestBuilder<SaveImageToFileWorker>()
+            .addTag(TAG_OUTPUT)
             .build()
+
         continuation = continuation.then(save)
         continuation.enqueue()
     }
@@ -57,7 +74,9 @@ class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
     /**
      * Cancel any ongoing WorkRequests
      * */
-    override fun cancelWork() {}
+    override fun cancelWork() {
+        workManager.cancelUniqueWork(IMAGE_MANIPULATION_WORK_NAME)
+    }
 
     /**
      * Creates the input data bundle which includes the blur level to
